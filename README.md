543e07da174c3d9001d98fd6e6098d8fe5c1c7d5
Rachid_Abdellaoui <rachidweb4@gmail.com>
2020-11-25 17:56:04 +0100

    lab

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# amazeing
+# AMAZEing
+
+Let's build a simple Maze program
+
+- Type of challenge: **consolidation**  
+- Duration: **2-3 days**  
+- Team challenge: **solo**
+- Deadline: **25/11/2020 19:00**
+- [Submission form](https://forms.gle/UmTTfyF59kueUYhh7)
+
+## Learning objectives
+
+Using loops and the DOM to generate simple templates. Using keyboard events
+
+## The mission
+
+- Create a new repository named `amazeing`
+- Create a `maze.js` file.
+- Create a file named `index.html`. In the `<body>` you can only put an empty `<main>` tag, and the link to your javascript file. The rest of the app has to be generated using your script
+- Use [this text file](lvl1.txt) as a reference to build a maze. `*` are walls, `.` are paths, `S` is the starting point fo the player and `T` is the treasure you should reach in order to end the game. 
+- Create a `maze.css` file:
+  - This file contains at `tile` class, a 20px by 20px square that will serve as a base for your maze
+  - You'll have to create styles for the 2 types of terrain (walls and paths), it can be simple background colors
+- Using the keyboard you can navigate in the maze (you can only stay on paths, you cannot cross walls)
+
+
+The maze map looks like this:
+
+![maze](map.png)
+
+## BONUS
+
+- Add more levels
+- Display the time spent to solve the maze
+- Make it pretty!
+
+## Starting tips
+
+To start this project I would store the maze in a variable first. You can create multiline string variables using backticks and split each lines into an array using the [String.split function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split)
+```javascript
+
+const multiline = `line1
+line2
+line3`
+
+console.log(multiline.split('\n')) // ['line1", 'line2', 'line3']
+console.log(multiline[0].split('')) // ['l', 'i', 'n', 'e', '1']
+```
+
+Something in the likes of `const mazeArray = maze.split('\n')` would allow me to iterate over the maze using for-loop
---
+++ maze.js
@@ -0,0 +1,124 @@
+const main = document.querySelector('main');
+
+
+const multiline =
+    `***********.*
+*S.....**.*.T
+*****.....*.*
+*****.***.*.*
+*****.*****.*
+*****.*****.*
+*****.......*
+*****.*******
+*.........***
+*.******...**
+*....********`
+
+const lab = document.createElement('div')
+lab.className = "lab"
+main.appendChild(lab)
+
+const joueur = document.createElement('div')
+joueur.className = 'joueur'
+
+
+for (let i = 0; i < multiline.length; i++) {
+
+    if (multiline[i].split('') == '*') {
+
+        const mur = document.createElement('div')
+        mur.className = "mur"
+        lab.appendChild(mur)
+
+    } else if (multiline[i].split('') == '.') {
+
+        const chemin = document.createElement('div')
+        chemin.id = i
+        chemin.className = "chemin"
+        lab.appendChild(chemin)
+
+    } else if (multiline[i].split('') == 'S') {
+
+        const debut = document.createElement('div')
+        debut.className = "debut"
+        debut.id = i
+        lab.appendChild(debut)
+        debut.appendChild(joueur)
+
+    } else if (multiline[i].split('') == 'T') {
+
+        const arriver = document.createElement('div')
+        arriver.className = "arriver"
+        arriver.id = i
+        lab.appendChild(arriver)
+    }
+}
+
+let joueurPosition = 15
+
+
+document.body.addEventListener('keyup', function (e) {
+
+    if (e.which == '38') {
+        haut()
+    } else if (e.which == '39') {
+        droite()
+    } else if (e.which == '37') {
+        gauche()
+    } else if (e.which == '40') {
+        bas()
+    }
+})
+
+function haut() {
+
+    let play = joueurPosition - 14
+
+    if (multiline[play].split('') != '*') {
+
+        joueurPosition = play
+        document.getElementById(joueurPosition).appendChild(joueur)
+
+
+
+    }
+}
+
+function bas() {
+
+    let play = joueurPosition + 14
+
+    if (multiline[play].split('') != '*') {
+
+        joueurPosition = play
+        document.getElementById(joueurPosition).appendChild(joueur)
+
+
+    }
+}
+
+function droite() {
+
+    let play = joueurPosition + 1
+
+    if (multiline[play].split('') != '*') {
+
+        joueurPosition = play
+        document.getElementById(joueurPosition).appendChild(joueur)
+
+
+    }
+}
+
+function gauche() {
+
+    let play = joueurPosition - 1
+
+    if (multiline[play].split('') != '*') {
+
+        joueurPosition = play
+        document.getElementById(joueurPosition).appendChild(joueur)
+
+
+    }
+}
